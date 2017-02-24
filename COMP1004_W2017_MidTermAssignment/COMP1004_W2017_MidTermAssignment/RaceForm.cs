cd48f7aa12a0daf0d5d30f59752b5375f9d8dc8a
anuroop <anuroop.reddy>
2017-02-24 14:56:45 -0500

    added resources folder and raceform

--- COMP1004_W2017_MidTermAssignment/COMP1004_W2017_MidTermAssignment/RaceForm.cs
+++ COMP1004_W2017_MidTermAssignment/COMP1004_W2017_MidTermAssignment/RaceForm.cs
@@ -12,9 +12,96 @@ namespace COMP1004_W2017_MidTermAssignment
 {
     public partial class RaceForm : Form
     {
+
+        public AbilityForm previousForm;
+        private string _selectedRace;        //instance variables
+
+        private int _StrengthMod = Convert.ToInt32(Program.character.STR);
+        private int _DexterityMod = Convert.ToInt32(Program.character.DEX);
+        private int _EnduranceMod = Convert.ToInt32(Program.character.END);
+        private int _IntellegenceMod = Convert.ToInt32(Program.character.INT);
+        private int _PerceptionMod = Convert.ToInt32(Program.character.PER);
+        private int _CharismaMod = Convert.ToInt32(Program.character.CHA);
+
+
         public RaceForm()
         {
             InitializeComponent();
         }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            Character character = Program.character;
+
+            character.Race = _selectedRace;
+
+
+            JobForm jobForm = new JobForm();
+            jobForm.previousForm = this;
+
+            jobForm.Show();
+            this.Hide();
+        }
+
+        private void HumanRadioButton_CheckedChanged_1(object sender, EventArgs e)
+        {
+            CharacterPictureBox.Image = Properties.Resources.Human;
+            RadioButton selectedRace = (RadioButton)sender;
+
+            RacialBonusTextBox.Text = "Increase all abilities by 10";
+
+            _StrengthMod = _StrengthMod + 5;
+            _DexterityMod = _DexterityMod + 5;
+            _EnduranceMod = _EnduranceMod + 5;
+            _IntellegenceMod = _IntellegenceMod + 5;
+            _PerceptionMod = _PerceptionMod + 5;
+            _CharismaMod = _CharismaMod + 5;
+
+
+            this._selectedRace = selectedRace.Text;
+        }
+
+        private void ElfRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+            CharacterPictureBox.Image = Properties.Resources.M_Elf1;
+            RadioButton selectedRace = (RadioButton)sender;
+            RacialBonusTextBox.Text = "Increase DEX and PER by 20";
+
+            _DexterityMod = _DexterityMod + 20;
+            _PerceptionMod = _PerceptionMod + 20;
+
+            this._selectedRace = selectedRace.Text;
+
+        }
+
+        private void DwarfRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            CharacterPictureBox.Image = Properties.Resources.M_Dwarf1;
+            RadioButton selectedRace = (RadioButton)sender;
+            RacialBonusTextBox.Text = "Increase STR and PER by 20, Decrease CHA by 10";
+
+            _StrengthMod = _StrengthMod + 20;
+            _PerceptionMod = _PerceptionMod + 20;
+            _CharismaMod = _CharismaMod - 10;
+            this._selectedRace = selectedRace.Text;
+
+        }
+
+        private void HalflingRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            CharacterPictureBox.Image = Properties.Resources.M_Halfling2;
+            RadioButton selectedRace = (RadioButton)sender;
+
+            RacialBonusTextBox.Text = "Increase DEX and INT by 20, Decrease STR by 10";
+            _DexterityMod = _DexterityMod + 20;
+            _IntellegenceMod = _IntellegenceMod + 20;
+            _StrengthMod = _StrengthMod - 10;
+
+            this._selectedRace = selectedRace.Text;
+
+        }
+
+
     }
 }
