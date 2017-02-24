85e5bd14a5eb87081c895a0b75475133c52a63e7
anuroop <anuroop.reddy>
2017-02-24 15:39:55 -0500

    implemented code in job form and created finalform

---
+++ COMP1004_W2017_MidTermAssignment/COMP1004_W2017_MidTermAssignment/FinalForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_W2017_MidTermAssignment
+{
+    public partial class FinalForm : Form
+    {
+        public JobForm previousForm { get; set; }
+
+        public FinalForm()
+        {
+            InitializeComponent();
+        }
+
+
+        //Calls the variable that fills all the read only text boxes with information gathered from other forms
+        //Done this way instead of one at a time to maximize cpu and uses less space.
+        private void FinalForm_Load(object sender, EventArgs e)
+        {
+            Stats();
+            if (raceTextBox.Text == "Human")
+            {
+                pictureBox.Image = Properties.Resources.M_Human1;
+            }
+            else if (raceTextBox.Text == "Elf")
+            {
+                pictureBox.Image = Properties.Resources.M_Elf1;
+            }
+            else if (raceTextBox.Text == "Dwarf")
+            {
+                pictureBox.Image = Properties.Resources.M_Dwarf1;
+            }
+            else if (raceTextBox.Text == "Halfling")
+            {
+                pictureBox.Image = Properties.Resources.M_Halfling2;
+            }
+        }
+
+        /// <summary>
+        /// this method sends the message to printer for printing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sending to Printer For Printing.....");
+        }
+
+        /// <summary>
+        /// this method helps to exit from the running program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        /// <summary>
+        /// this method changes the font of the labels and the text box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+
+            fontDialog.ShowDialog();
+            textBox1.Font = fontDialog.Font;
+            textBox2.Font = fontDialog.Font;
+            textBox3.Font = fontDialog.Font;
+            textBox4.Font = fontDialog.Font;
+            jobTextBox.Font = fontDialog.Font;
+            raceTextBox.Font = fontDialog.Font;
+            healthTextBox.Font = fontDialog.Font;
+            nameLabel.Font = fontDialog.Font;
+            ageLabel.Font = fontDialog.Font;
+            heightLabel.Font = fontDialog.Font;
+            weightLabel.Font = fontDialog.Font;
+            jobLabel.Font = fontDialog.Font;
+            raceLabel.Font = fontDialog.Font;
+            healthPointsLabel.Font = fontDialog.Font;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox AboutBox = new AboutBox();
+
+            AboutBox.ShowDialog();
+        }
+
+        public void Stats()
+        {
+            healthTextBox.Text = Program.character.Health;
+            raceTextBox.Text = Program.character.Race;
+            jobTextBox.Text = Program.character.Job;
+            STRTextBox.Text = Program.character.STR;
+            DEXTextBox.Text = Program.character.DEX;
+            ENDTextBox.Text = Program.character.END;
+            INTTextBox.Text = Program.character.INT;
+            PERTextBox.Text = Program.character.PER;
+            CHATextBox.Text = Program.character.CHA;
+        }
+    }
+}
--- COMP1004_W2017_MidTermAssignment/COMP1004_W2017_MidTermAssignment/JobForm.cs
+++ COMP1004_W2017_MidTermAssignment/COMP1004_W2017_MidTermAssignment/JobForm.cs
@@ -12,34 +12,60 @@ namespace COMP1004_W2017_MidTermAssignment
 {
     public partial class JobForm : Form
     {
+
+        public RaceForm previousForm;
+
+        private int _soldierHealth = 30;
+        private int _rogueHealth = 28;
+        private int _magickerHealth = 15;
+        private int _cultistHealth = 24;
+
+
+        private int _DexterityHealthMod = Convert.ToInt32(Program.character.DEX);
+        private int _EnduranceHealthMod = Convert.ToInt32(Program.character.END);
+        private int _IntellegenceHealthMod = Convert.ToInt32(Program.character.INT);
+        private int _CharismaHealthMod = Convert.ToInt32(Program.character.CHA);
+
+
+
         public JobForm()
         {
             InitializeComponent();
         }
 
-        private void CultistRadioButton_CheckedChanged(object sender, EventArgs e)
+        private void NextButton_Click(object sender, EventArgs e)
         {
+            Character character = Program.character;
 
-        }
+            character.Health = healthPointTextBox.Text;
 
-        private void MagickerRadioButton_CheckedChanged(object sender, EventArgs e)
-        {
+            this.Hide();
 
-        }
+            FinalForm finalForm = new FinalForm();
 
-        private void RogueRadioButton_CheckedChanged(object sender, EventArgs e)
-        {
+            finalForm.previousForm = this;
 
+            finalForm.Show();
         }
 
         private void SoldierRadioButton_CheckedChanged(object sender, EventArgs e)
         {
-
+            healthPointTextBox.Text = (_soldierHealth + _EnduranceHealthMod).ToString();
         }
 
-        private void NextButton_Click(object sender, EventArgs e)
+        private void RogueRadioButton_CheckedChanged(object sender, EventArgs e)
         {
+            healthPointTextBox.Text = (_rogueHealth + _DexterityHealthMod).ToString();
+        }
 
+        private void MagickerRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            healthPointTextBox.Text = (_magickerHealth + _IntellegenceHealthMod).ToString();
+        }
+
+        private void CultistRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            healthPointTextBox.Text = (_cultistHealth + _CharismaHealthMod).ToString();
         }
     }
 }
